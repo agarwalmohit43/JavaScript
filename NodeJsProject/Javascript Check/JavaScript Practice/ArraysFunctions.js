ba78f8ae500899dff491b6c5de9e7a417469daa3
agarwalmohit43 <agarwalmohit43@gmail.com>
2017-09-02 17:13:14 +0530

    callbacks and array Function added

---
+++ NodeJsProject/Javascript Check/JavaScript Practice/ArraysFunctions.js
@@ -0,0 +1,29 @@
+var arr = [10,2,8,1,15,12];
+var arr2 = ["Mohit","Yash","Aishs","ilu","apple"];
+var arr3 = ["Mohit","25","Letter",10,50];
+
+// console.log(arr.concat(arr2).concat(arr3).toString());//concat
+// console.log(arr2.copyWithin(4,0,2));//copyWithin(target,start,end)
+// console.log(arr2.every(function (t) { return !isNaN(t); }));// every checks every elements in array pass the test. returns boolean
+// console.log(arr2.fill("Yoo",1,5));//replace the character and fill with the value passed from index position to real position.
+// console.log(arr3.filter(function (t) { return !isNaN(t); }));//filters out the elements from arrays as per requirements.
+// console.log(arr.find(function (t) { return t<5;})) // return first find conditioned matched element from array
+// console.log(arr.findIndex(function (t) { return t<5;})) // return first find conditioned matched element index from array
+// console.log(arr.indexOf(10));//returns index of present element else return -1;
+// console.log(Array.isArray(arr));
+// console.log(arr.join(','));//joins each element with separators; return string
+// arr.forEach(function (t) { return Math.pow(t,2);  });
+// console.log(arr.lastIndexOf(2));
+// console.log(arr.map(function (t) { return Math.pow(t,2); })); creates new array according to the condition of callback.
+// console.log(arr.pop());
+// console.log(arr.push(12));
+// console.log(arr.reduce(function (sum,i) { return sum+i; }));//adds all the element of the array
+//  console.log(arr3.reduceRight(function (sum,i) { return sum+i; }));//adds all the element of the array from right
+// console.log(arr.reverse());//reverse the array elements
+// console.log(arr.shift());//removes 1st element of array and return it;
+// console.log(arr.unshift(10));add element at first and returns total element in array
+// console.log(arr.slice(0,1));// cut the array and return middle elements;
+// console.log(arr.splice(1,4,"d"));//splice(start,end,inserting)
+// console.log(arr2.some(function (t) { return !isNaN(t); }))//just opposite of every if any element pass the test
+// console.log(arr2.toString());//converts array to string
+// console.log(arr.valueOf());// return primitive value of the array
--- NodeJsProject/Javascript Check/JavaScript Practice/one.js
+++ NodeJsProject/Javascript Check/JavaScript Practice/one.js
@@ -99,6 +99,12 @@ function strings() {
     console.log(name.toUpperCase());
     console.log(name.trim());
     console.log(name.slice(0,3));//substring
+
+    var string = "Mohit";
+    var str = new String("Mohit");
+    console.log(string == str);
+    console.log(string === str);
+    console.log(typeof string +" <-> "+ typeof str);
 }
 
 function dates() {
@@ -240,10 +246,81 @@ function checkies() {
     addTen(2);
 }
 
-var check = 5;
-function sada() {
+function checkies2() {
     var check = 10;
-    console.log(window.check);
+    console.log(parseInt('foo'));
+    var arr = ['foo',5,5,'baz',10,'8'];
+
+    var sum = 0;
+
+    for(var i=0;i<arr.length;i++)
+    {
+        if(!isNaN(arr[i]))
+        {
+            sum+=arr[i];
+        }else
+        {
+            var number=parseInt(arr[i]);
+            if(isNaN(number))
+                continue;
+            else
+                sum+=number;
+        }
+    }
+    console.log(sum);
+    //console.log(arr.map(function(i){ return parseInt(i);}).filter(function(x) { return !isNaN(x) }));
+    console.log(arr.map(function(i){ return parseInt(i);}).filter(function(x) { return !isNaN(x) }).reduce(function(sum,i) { return sum + i} ));
 }
 
-sada();
+// var arr = [10,8,1,2];
+// arr.sort(function (a,b) {
+//     console.log("Numbers are:"+a+" "+b);
+//     return a>b;
+// })
+// console.log(arr);
+
+// var arr2 = [{"name":"abc","age":"50"},{"name":"def","age":"20"},{"name":"ghi","age":"40"},{"name":"jkl","age":"35"},{"name":"mno","age":"15"}];
+//
+//
+// arr2.sort(function (a,b) {
+//         return a.age < b.age;
+// });
+// console.log(arr2);
+
+// var arr = ["Mohit",1,2,3,4,5];
+// console.log(arr.map(function (t) { return parseInt(t); }).filter(function (t) { return !isNaN(t); }));
+//console.log(arr.map(function (t) { return parseInt(t); }).filter(function (t) { return !isNaN(t); }).reduce(function (sum,i) { return sum+i;  }));
+//console.log(arr.filter(function (t) { return !isNaN(t); }).reduce(function (sum,i) { return sum+i;  }));
+
+
+
+// function add(a,b){ return a+b};
+// function asda(a,b,callback) {
+//    return callback(a,b);
+// }
+//
+// console.log(asda(5,4,add));
+
+// var arr = ['foo',5,5,'baz',10,'8'];
+//
+// var sum = 0;
+//
+// for(var i=0;i<arr.length;i++)
+// {
+//     if(!isNaN(arr[i]))
+//     {
+//
+//         sum+=parseInt(arr[i]);
+//     }else
+//     {
+//         var number=parseInt(arr[i]);
+//         console.log(number);
+//         if(isNaN(number))
+//             continue;
+//         else
+//         {
+//             sum+=number;
+//         }
+//     }
+// }
+// console.log(sum);
