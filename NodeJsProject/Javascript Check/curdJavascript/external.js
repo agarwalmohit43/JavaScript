6332b1354cbfe1414477897b26068e4a8043f6c1
agarwalmohit43 <agarwalmohit43@gmail.com>
2017-09-04 18:31:31 +0530

    updated CRUD

--- NodeJsProject/Javascript Check/curdJavascript/external.js
+++ NodeJsProject/Javascript Check/curdJavascript/external.js
@@ -1,21 +1,39 @@
 var arr = new Array();
 
 var currentRow=0;
+
+
 function formData()
 {
+    var btnvalue = document.getElementById("empSubmit").value;
+    var cancelBtn = document.getElementById("empCancel").value;
 
-     var empId = document.getElementById("empId").value;
-     var empName = document.getElementById("empName").value;
+    // debugger;
+    // console.log(btnvalue);
 
-    arr.push({empId:empId,empName:empName});
+    if(btnvalue === 'Submit')
+    {
+        var empId = document.getElementById("empId").value;
+        var empName = document.getElementById("empName").value;
 
-    console.log(empId + " " + empName);
-    console.log(arr);
+        arr.push({empId:empId,empName:empName});
 
-    addElementsInArray();
+        console.log(empId + " " + empName);
+        console.log(arr);
+        console.log(btnvalue);
+        addElementsInArray();
+        document.getElementById("empId").value="";
+        document.getElementById("empName").value="";
+    }else if(btnvalue === 'Update')
+    {
+        updateFormData();
+    }
 
-    document.getElementById("empId").value="";
-    document.getElementById("empName").value="";
+    if(cancelBtn === 'Cancel')
+    {
+        cancel();
+    }
+
 }
 
 function editEmp(e){
@@ -26,16 +44,23 @@ function editEmp(e){
      document.getElementById("empName").value=arr[e-1].empName;
      currentRow=e;
 
-     document.getElementById("empUpdate").style.visibility="visible";
-     document.getElementById("empUpdateCancel").style.visibility="visible";
-     document.getElementById("empSubmit").style.visibility="hidden";
+
+    document.getElementById("empSubmit").value = "Update";
+    document.getElementById("empCancel").style.visibility="visible";
+
+    // document.getElementById("empUpdate").style.visibility="visible";
+    // document.getElementById("empUpdateCancel").style.visibility="visible";
+    // document.getElementById("empSubmit").style.visibility="hidden";
 }
 
 function updateFormData() {
     console.log(currentRow);
-    document.getElementById("empSubmit").style.visibility="visible";
-    document.getElementById("empUpdate").style.visibility="hidden";
-    document.getElementById("empUpdateCancel").style.visibility="hidden";
+
+    document.getElementById("empSubmit").value = "Submit";
+
+    // document.getElementById("empSubmit").style.visibility="visible";
+    // document.getElementById("empUpdate").style.visibility="hidden";
+    // document.getElementById("empUpdateCancel").style.visibility="hidden";
 
     var empId = document.getElementById("empId").value;
     var empName = document.getElementById("empName").value;
@@ -59,9 +84,12 @@ function cancel() {
     currentRow=0;
     document.getElementById("empId").value="";
     document.getElementById("empName").value="";
-    document.getElementById("empUpdate").style.visibility="hidden";
-    document.getElementById("empUpdateCancel").style.visibility="hidden";
-    document.getElementById("empSubmit").style.visibility="visible";
+
+    document.getElementById("empCancel").style.visibility="hidden";
+
+    // document.getElementById("empUpdate").style.visibility="hidden";
+    // document.getElementById("empUpdateCancel").style.visibility="hidden";
+    // document.getElementById("empSubmit").style.visibility="visible";
 }
 
 function deleteAllrows() {
@@ -74,7 +102,9 @@ function deleteAllrows() {
 
 function addElementsInArray() {
    deleteAllrows();
-    for(var i in arr){
+
+   for(var i in arr)
+    {
         var table = document.getElementById('empTable').getElementsByTagName('tbody')[0];
         var row = table.insertRow(table.rows.length);
         var cell1=row.insertCell(0);
