0d687c081393df23a6861ceb56537ac004845a8a
agarwalmohit43 <agarwalmohit43@gmail.com>
2017-09-05 01:21:25 +0530

    updated CRUD

---
+++ NodeJsProject/Javascript Check/Jquery/external.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+   // alert("Hi");
+
+    // $('#check').click(function () {
+    //     $('#check').hide(2000,function () {
+    //         $('#check').show(2000,function () {
+    //             alert('aagaya');
+    //         });
+    //     });})
+
+    $('#check').click(function () {
+
+            $('#textId').css('color','red').slideUp(1000).slideDown(1000);
+
+
+    })
+
+});
+
