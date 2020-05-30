fa610044bf3852f31822f95c2fc60f5892c42473
agarwalmohit43 <agarwalmohit43@gmail.com>
2020-05-30 11:59:02 +0530

    Practice session

--- Prac2.0/Array/practice.js
+++ Prac2.0/Array/practice.js
@@ -10,3 +10,22 @@ console.log(arr5.includes(5)); // return true/false
 
 let str = 'The quick brown fox jumps over the lazy dog.';
 console.log(`The \'word\' ${str.includes('work') ? 'is' : 'is not'} present in => ${str}`)
+
+//remove duplicates
+let dupArr = [7,2,1,2,5,1];
+dupArr = [...dupArr,7];
+let removeDup = dupArr.filter((item1, index) => dupArr.indexOf(item1) === index)
+console.log(removeDup)
+
+console.log(`Using Set: ${[...new Set(dupArr)]}`);
+
+let removeDup2 = dupArr.reduce((a,b) => {
+    if (a.indexOf(b) < 0) a.push(b)
+    return a;
+}, [])
+console.log(removeDup2)
+
+let removeDup3 = dupArr.reduce((a,b) => (a.includes(b)) ? a: [...a, b], [])
+
+console.log(removeDup3)
+
